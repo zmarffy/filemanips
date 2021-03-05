362c76ba892e1aba526bb955aa7fbd772cc2afc4
Zeke Marffy <zmarffy@me.com>
2021-03-05 23:16:41 +0000

    remove old code

--- setup.py
+++ setup.py
@@ -1,23 +1,22 @@
-import setuptools
-import zetuptools
+import os
+import re
 
-TOOLS = zetuptools.SetuptoolsExtensions(
-    "filemanips", "Zeke Marffy", "zmarffy@yahoo.com")
+import setuptools
+
+with open(os.path.join("filemanips", "__init__.py"), encoding="utf8") as f:
+    version = re.search(r'__version__ = "(.*?)"', f.read()).group(1)
 
 
 setuptools.setup(
-    name=TOOLS.name,
-    version=TOOLS.version,
-    author=TOOLS.author,
-    author_email=TOOLS.author_email,
+    name="filemanips",
+    version=version,
+    author="Zeke Marffy",
+    author_email="zmarffy@yahoo.com",
     packages=setuptools.find_packages(),
     url='https://github.com/zmarffy/filemanips',
     license='MIT',
     description='File manipulation functions',
-    python_requires=TOOLS.minimum_version_required,
+    python_requires='>=3.8',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
-    install_requires=[
-        "zetuptools>=2.3.0"
-    ],
 )
